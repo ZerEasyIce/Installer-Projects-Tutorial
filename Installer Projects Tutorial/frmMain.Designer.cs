337d7611593dc385e342a9a6fa848ea96aad2fb9
ZerEasyIce <golfburintorn@gmail.com>
2025-06-05 00:21:45 +0700

    Add project files.

---
+++ Installer Projects Tutorial/frmMain.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Installer_Projects_Tutorial
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.lblText = new System.Windows.Forms.Label();
+            this.pnlContent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(50, 192);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(91, 44);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.TabStop = false;
+            this.btnRed.Text = "RED";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.Location = new System.Drawing.Point(147, 192);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(91, 44);
+            this.btnYellow.TabIndex = 0;
+            this.btnYellow.TabStop = false;
+            this.btnYellow.Text = "YELLOW";
+            this.btnYellow.UseVisualStyleBackColor = true;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(244, 192);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(91, 44);
+            this.btnBlue.TabIndex = 0;
+            this.btnBlue.TabStop = false;
+            this.btnBlue.Text = "BLUE";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlContent.Controls.Add(this.lblText);
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlContent.Location = new System.Drawing.Point(10, 20);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Padding = new System.Windows.Forms.Padding(1);
+            this.pnlContent.Size = new System.Drawing.Size(364, 147);
+            this.pnlContent.TabIndex = 1;
+            // 
+            // lblText
+            // 
+            this.lblText.BackColor = System.Drawing.SystemColors.Info;
+            this.lblText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblText.Location = new System.Drawing.Point(1, 1);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(362, 145);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Click the button to change the color";
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnRed);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(10, 20, 10, 0);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Installer Projects Tutorial";
+            this.pnlContent.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.Label lblText;
+    }
+}
+
---
+++ Installer Projects Tutorial/frmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Installer_Projects_Tutorial
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            lblText.BackColor = Color.Red;
+            lblText.Focus();
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            lblText.BackColor = Color.Yellow;
+            lblText.Focus();
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            lblText.BackColor = Color.Blue;
+            lblText.Focus();
+        }
+    }
+}
